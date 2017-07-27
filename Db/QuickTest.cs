c1dbaae9e679be42bda54d80ab825fde8e1bb6d3
josh-jhs8 <josh.jhs8@gmail.com>
2017-07-27 18:09:38 +0100

    Get Code First Database started (including a quick test function)

---
+++ Db/QuickTest.cs
@@ -0,0 +1,33 @@
+﻿using DbModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Db
+{
+    public class QuickTest
+    {
+        public static void TestDb()
+        {
+            using (var db = new W40kDbContext())
+            {
+                var unitType = new UnitType { Name = "Elite" };
+
+                db.UnitTypes.Add(unitType);
+                db.SaveChanges();
+
+                var query = db.UnitTypes.OrderBy(ut => ut.Name);
+
+                foreach (var item in query)
+                {
+                    Debug.WriteLine(item.Name);
+                }
+
+            }
+        }
+    }
+}
--- w40k-model/MainWindow.xaml.cs
+++ w40k-model/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Db;
 
 namespace w40k_model
 {
@@ -23,6 +24,7 @@ namespace w40k_model
         public MainWindow()
         {
             InitializeComponent();
+            QuickTest.TestDb();
         }
     }
 }
