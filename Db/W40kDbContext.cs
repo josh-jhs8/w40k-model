a0d2267ed781a0ad118c81f96a137934725d1da4
josh-jhs8 <josh.jhs8@gmail.com>
2017-07-29 10:58:51 +0100

    Add faction table

--- Db/W40kDbContext.cs
+++ Db/W40kDbContext.cs
@@ -11,5 +11,7 @@ namespace Db
     public class W40kDbContext : DbContext
     {
         public DbSet<UnitType> UnitTypes { get; set; }
+
+        public DbSet<Faction> Factions { get; set; }
     }
 }
